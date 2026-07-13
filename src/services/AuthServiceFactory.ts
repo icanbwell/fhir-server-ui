14a2f676a2940136c091d5ec8435ee8197920f8a
Mintu Kumar Sah <mintu.sah@icanbwell.com>
2026-07-13 12:35:37 +0530

    Remove bwell case from AuthServiceFactory

--- src/services/AuthServiceFactory.ts
+++ src/services/AuthServiceFactory.ts
@@ -16,8 +16,6 @@ class AuthServiceFactory {
                 return new OktaAuthService();
             case 'cognito':
                 return new CognitoAuthService();
-            case 'bwell':
-                return new CognitoAuthService();
             default:
                 throw new Error(`Unsupported identity provider: ${identityProvider}`);
         }
